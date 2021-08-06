c2d11438b1019ba0620d43ba7a717f03f5850fec
SHJohnson <shjohnson413@gmail.com>
2021-08-05 18:48:06 -0700

    added RenderComments component

--- components/regionDetailComponent.js
+++ components/regionDetailComponent.js
@@ -1,7 +1,10 @@
 import React, { Component }  from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView, FlatList } from 'react-native';
 import { Card } from 'react-native-elements';
+import RenderComments from './RenderComments';
 import { REGIONS_HISTORY } from '../shared/regionsHistory';
+import { REGION_COMMENTS } from '../shared/regionComments';
+
 
 function RenderRegionDetail({region}) {
     if (region) {
@@ -23,7 +26,8 @@ class RegionDetail extends Component  {
     constructor(props){
         super(props);
         this.state ={
-            regions: REGIONS_HISTORY
+            regions: REGIONS_HISTORY,
+            comments: REGION_COMMENTS
         }
     }
 
@@ -33,8 +37,14 @@ class RegionDetail extends Component  {
 
     render(){
         const regionId = this.props.navigation.getParam('regionId');
+        const comments = this.state.comments.filter(comment => comment.regionId === regionId);
         const region = this.state.regions.filter(region => region.id === regionId)[0];
-        return <RenderRegionDetail region={region}  />;
+        return (
+            <ScrollView>
+                <RenderRegionDetail region={region}  />
+                <RenderComments comments={comments} />
+            </ScrollView>
+        );
     }
 }
 
---
+++ shared/regionComments.js
@@ -0,0 +1,163 @@
+export const REGION_COMMENTS = 
+[
+    {
+        id: 0,
+        regionId: 0,
+        rating: 5, 
+        text: "What a magnificent view!",
+        author: "Jules Vern",
+        date: "2018-10-25T16:30Z"
+    },
+    {
+        id: 1,
+        regionId: 0,
+        rating: 4, 
+        text: "What a magnificent community!",
+        author: "Jules Vern",
+        date: "2017-06-17T03:33Z"
+    },
+    {
+        id: 2,
+        regionId: 0,
+        rating: 5, 
+        text: "I caught a world-record-breaking trout here on my last visit.",
+        author: "Jules Vern",
+        date: "2019-02-18T18:12Z"
+    },
+    {
+        id: 3,
+        regionId: 0,
+        rating: 5, 
+        text: "I caught rebugs, stay out of the jungle...",
+        author: "Jules Vern",
+        date: "2019-08-04T20:11Z"
+    },
+    {
+        id: 4,
+        regionId: 0,
+        rating: 4, 
+        text: "Nice people, beautiful landscape. Did the local dance all night!",
+        author: "Jules Vern",
+        date: "2018-07-23T19:44Z"
+    },
+    {
+        id: 5,
+        regionId: 1,
+        rating: 5, 
+        text: "What a magnificent view!",
+        author: "Jules Vern",
+        date: "2018-10-25T16:30Z"
+    },
+    {
+        id: 6,
+        regionId: 1,
+        rating: 4, 
+        text: "What a magnificent community!",
+        author: "Jules Vern",
+        date: "2017-06-17T03:33Z"
+    },
+    {
+        id: 7,
+        regionId: 1,
+        rating: 5, 
+        text: "I caught a world-record-breaking trout here on my last visit.",
+        author: "Jules Vern",
+        date: "2019-02-18T18:12Z"
+    },
+    {
+        id: 8,
+        regionId: 1,
+        rating: 5, 
+        text: "I caught rebugs, stay out of the jungle...",
+        author: "Tinus Lorvaldes",
+        date: "2019-08-04T20:11Z"
+    },
+    {
+        id: 9,
+        regionId: 1,
+        rating: 4, 
+        text: "Nice people, beautiful landscape. Did the local dance all night!",
+        author: "Jules Vern",
+        date: "2018-07-23T19:44Z"
+    },
+    {
+        id: 10,
+        regionId: 2,
+        rating: 5, 
+        text: "What a magnificent view!",
+        author: "Jules Vern",
+        date: "2018-10-25T16:30Z"
+    },
+    {
+        id: 11,
+        regionId: 2,
+        rating: 4, 
+        text: "What a magnificent community!",
+        author: "Jules Vern",
+        date: "2017-06-17T03:33Z"
+    },
+    {
+        id: 12,
+        regionId: 2,
+        rating: 5, 
+        text: "I caught a world-record-breaking trout here on my last visit.",
+        author: "Jules Vern",
+        date: "2019-02-18T18:12Z"
+    },
+    {
+        id: 13,
+        regionId: 2,
+        rating: 5, 
+        text: "I caught rebugs, stay out of the jungle...",
+        author: "Jules Vern",
+        date: "2019-08-04T20:11Z"
+    },
+    {
+        id: 14,
+        regionId: 2,
+        rating: 4, 
+        text: "Nice people, beautiful landscape. Did the local dance all night!",
+        author: "Jules Vern",
+        date: "2018-07-23T19:44Z"
+    },
+    {
+        id: 15,
+        regionId: 3,
+        rating: 5, 
+        textt: "What a magnificent view!",
+        author: "Jules Vern",
+        date: "2018-10-25T16:30Z"
+    },
+    {
+        id: 16,
+        regionId: 3,
+        rating: 4, 
+        text: "What a magnificent community!",
+        author: "Jules Vern",
+        date: "2017-06-17T03:33Z"
+    },
+    {
+        id: 17,
+        regionId: 3,
+        rating: 5, 
+        text: "I caught a world-record-breaking trout here on my last visit.",
+        author: "Jules Vern",
+        date: "2019-02-18T18:12Z"
+    },
+    {
+        id: 18,
+        regionId: 3,
+        rating: 5, 
+        text: "I caught rebugs, stay out of the jungle...",
+        author: "Jules Vern",
+        date: "2019-08-04T20:11Z"
+    },
+    {
+        id: 19,
+        regionId: 3,
+        rating: 4, 
+        text: "Nice people, beautiful landscape. Did the local dance all night!",
+        author: "Jules Vern",
+        date: "2018-07-23T19:44Z"
+    }
+]
